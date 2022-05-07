512e30f7af8755692d403d8fb2d2f6b78fd1a815
ceddadh <cedanik1@gmail.com>
2022-05-07 16:10:09 +0200

    initial setup

---
+++ discord-backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+
+const PORT = process.env.PORT || process.env.API_PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  })
+  .catch((err) => {
+    console.log("Databse connection failed. Server is not running.");
+    console.log(err);
+  });
